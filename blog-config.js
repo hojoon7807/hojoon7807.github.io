2b84ee55227ecbe1dc45592e1b66a1b44c59c54e
hojoon <ujuj0202@gmail.com>
2022-01-28 04:51:39 +0900

    test

---
+++ blog-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "Dev.HJ",
+  description: "Devolopment Blog",
+  author: "Hojoon Lim",
+  siteUrl: "https://hojoon7807.github.io",
+  links: {
+    github: "https://github.com/hojoon7807",
+    email: "ujuj0202@gmail.com",
+  },
+  utterances: {
+    repo: "hojoon7807/hojoon7807.github.io",
+    type: "pathname",
+  },
+}
---
+++ contents/posts/test/index.md
@@ -0,0 +1,11 @@
+---
+title: "test"
+description: "test"
+date: 2022-01-28
+update: 2022-01-28
+tags:
+  - test
+  - blog
+---
+
+# test
